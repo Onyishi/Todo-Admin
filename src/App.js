28184b4973699781bba137c67101e143bc30a07f
Chetachi <vichetachi@gmail.com>
2021-08-24 04:54:15 +0100

    the repo was created and all dependencies were installed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { ShowTodoList } from "./components/showTodoList"
+import './App.scss' // updated
+
+function App () {
+  return (
+    <div className='App'>
+      <header>Hello, World</header>
+
+    </div>
+  )
+}
+
+export default App
