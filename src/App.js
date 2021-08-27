059a260366f8b8430856a4989c25da5b3fe04778
Chetachi <vichetachi@gmail.com>
2021-08-27 12:43:12 +0100

    debuged the necessary components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route } from 'react-router-dom'
 import { ShowTodoList } from './components/showTodoList'
+import { createTodo } from './components/createTodo'
 import './App.scss' // updated
 
 function App () {
   return (
     <div className='App-contents'>
+      <BrowserRouter>
+        <Route exact path='/' component={ShowTodoList} />
+        <Route path='/create-todo' component={createTodo} />
+      </BrowserRouter>
       <ShowTodoList />
     </div>
   )
